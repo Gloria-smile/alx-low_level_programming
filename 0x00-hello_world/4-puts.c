480083bccd3229f10dca6cb1ab5c57dfacb3411f
Gloria-smile <gloriaagiamah@gmail.com>
2023-03-20 07:56:44 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,5 +11,15 @@
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle,");
+/* Verify output */
+printf("[Got] Programming is like building a multilingual puzzle,\n");
+printf("(%d chars long)\n", 52);
+printf("[stderr]: ");
+printf("(%d chars long)\n", 0);
+/* Expected output */
+printf("[Expected] \"Programming is like building a multilingual puzzle,\n");
+printf("(%d chars long)\n", 52);
+printf("[stderr]: [Anything]\n");
+printf("(%d chars long)\n", 0);
 return (0);
 }
