ab3fd02af01960cbfc01d91c0bb783383ceb482b
Gloria-smile <gloriaagiamah@gmail.com>
2023-03-20 12:42:21 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
  * main - Entry point of the program
  *
  * This function prints the message "Programming is like building
- * a multilingual puzzle," to the console and returns 0 to indicate
+ * a multilingual puzzle" to the console and returns 0 to indicate
  * successful execution.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle,");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
