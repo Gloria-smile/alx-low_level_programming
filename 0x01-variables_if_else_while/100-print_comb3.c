7a8c0c0c9d671157009f2431c5009316c0990643
Gloria-smile <gloriaagiamah@gmail.com>
2023-03-21 06:13:08 -0700

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,15 @@
-include <stdio.h>
+#include <stdio.h>
 /**
-* main - prints all possible different combinations of two digits
-* Return: ALways 0 (Success)
+* main - prints the lowercase alphabet in reverse,
+* followed by a new line
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-int n, m;
-for (n = 48; n <= 56; n++)
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
 {
-for (m = 49; m <= 57; m++)
-{
-if (m > n)
-{
-putchar(n);
-putchar(m);
-if (n != 56 || m != 57)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
+putchar(ch);
 }
 putchar('\n');
 return (0);
