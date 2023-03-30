1b6ed0c39d3af87a678976f5f19e4fcdea5e7f22
Gloria-smile <gloriaagiamah@gmail.com>
2023-03-29 23:50:34 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,7 @@
  * print_rev - imprime en reversa
  * @s: string
  * return: 0
-*/
+ */
 void print_rev(char *s)
 {
 	int longi = 0;
@@ -17,7 +17,7 @@ void print_rev(char *s)
 	s--;
 	for (o = longi; o > 0; o--)
 	{
-	_putschar(*s);
+	_putchar(*s);
 	s--;
 	}
 
