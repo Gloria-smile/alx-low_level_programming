ed36c5c4ef1c4741bc0d32e7a242edc6513c8e00
Gloria-smile <gloriaagiamah@gmail.com>
2023-04-01 10:33:24 -0700

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,8 +12,10 @@ void print_times_table(int n)
 	for (i = 0; i <= n; i++)
 	{
 	for (j = 0; j <= n; j++)
-	{
+	{	
 	k = j * i;
+	}
+	}
 	if (j == 0)
 	{
 	_putchar(k + '0');
@@ -44,3 +46,5 @@ void print_times_table(int n)
 	_putchar('\n');
 	}
 }
+}
+
