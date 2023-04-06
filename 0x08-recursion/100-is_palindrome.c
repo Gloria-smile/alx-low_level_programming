ed8d9b473b6a2aceb77effeb389ff6961e8225d8
Gloria-smile <gloriaagiamah@gmail.com>
2023-04-06 07:14:18 -0700

    7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: pointer to the string to check
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s);
+
+/**
+ * check_palindrome - checks if a string is a palindrome recursively
+ * @s: pointer to the string to check
+ * @start: starting index of the string to check
+ * @end: ending index of the string to check
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int check_palindrome(char *s, int start, int end);
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: pointer to the string to check
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (check_palindrome(s, 0, len - 1));
+}
+
+/**
+ * check_palindrome - checks if a string is a palindrome recursively
+ * @s: pointer to the string to check
+ * @start: starting index of the string to check
+ * @end: ending index of the string to check
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+	return (1);
+
+	if (s[start] != s[end])
+	return (0);
+
+	return (check_palindrome(s, start + 1, end - 1));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to the string to get the length of
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	return (0);
+	else
+
+	return (1 + _strlen_recursion(s + 1));
+}
