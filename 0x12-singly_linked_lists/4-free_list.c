0bc114bf051a63029121fd5db97f46c667028dd7
Gloria-smile <gloriaagiamah@gmail.com>
2023-04-28 06:37:58 -0700

    4.freeList

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the start of the list
+ *
+ * Return: void
+ * Author - Gloria Agiamah
+ * Date - 28-04-23
+ */
+void free_list(list_t *head)
+{
+	list_t *current, *next;
+
+	current = head;
+	while (current)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+
+}
