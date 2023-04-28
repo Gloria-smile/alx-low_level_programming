c733da2091247c139199dac2b95bcabff0decae9
Gloria-smile <gloriaagiamah@gmail.com>
2023-04-28 05:57:39 -0700

    0.printlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,16 +3,16 @@
 
 /**
  * print_list - prints all the elements of a list_t list
- * @begin: pointer to the start of the list
+ * @h: pointer to the start of the list
  *
  * Return: number of nodes in the list
  */
-size_t print_list(const list_t *begin)
+size_t print_list(const list_t *h)
 {
 	const list_t *tmp;
 	unsigned int i;
 
-	tmp = begin;
+	tmp = h;
 	for (i = 0; tmp; i++)
 	{
 	printf("[%u] %s\n", tmp->len, tmp->str);
