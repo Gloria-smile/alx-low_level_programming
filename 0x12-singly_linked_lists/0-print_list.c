0de8fc3f27f23ad8d9b3dc37a8c4f192c23cdd7e
Gloria-smile <gloriaagiamah@gmail.com>
2023-04-28 18:00:16 -0700

    0.printList

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	const list_t *tmp;
-	unsigned int i;
+	size_t tmp;
 
-	tmp = h;
-	for (i = 0; tmp; i++)
+	tmp = 0;
+
+	while (h != NULL)
 	{
-		printf("[%u] %s\n", tmp->len, tmp->str);
-		tmp = tmp->next;
+	if (h->str == NULL)
+	printf("[%d} %s\n", 0, "(nil)");
+	else
+	printf("[%d] %s\n", h->len, h->str);
+	h = h->next;
+	tmp++;
 	}
-	return (i);
+	return (tmp);
 }
