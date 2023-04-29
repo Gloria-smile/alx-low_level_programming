c63bf467b9b47980a50d7ac6f00ced80900b3859
Gloria-smile <gloriaagiamah@gmail.com>
2023-04-29 07:46:26 -0700

    2.collaborationismultiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result (product of two integers)
+ * Author - Gloria Agiamah
+ * Date - 29-04-23
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
